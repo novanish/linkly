05fbca938136d2798a3d9d6714bfe5c16d312588
novanish <anishchhetri196@gmail.com>
2025-05-19 18:52:40 +0545

    feat: add route config

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,37 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  prefix,
+  route,
+} from '@react-router/dev/routes';
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index('routes/home.tsx'),
+
+  layout(
+    'routes/auth/layout.tsx',
+    prefix('auth', [
+      route('login', 'routes/auth/login.tsx'),
+      route('logout', 'routes/auth/logout.tsx'),
+      route('verify-magic-link', 'routes/auth/magic-link/verify.tsx'),
+      ...prefix('google', [
+        route('login', 'routes/auth/google/login.tsx'),
+        route('callback', 'routes/auth/google/callback.tsx'),
+      ]),
+    ]),
+  ),
+
+  layout(
+    'routes/dashboard/layout.tsx',
+    prefix('dashboard', [
+      route('overview', 'routes/dashboard/overview.tsx'),
+      route('links', 'routes/dashboard/links.tsx'),
+      route('analytics', 'routes/dashboard/analytics.tsx'),
+    ]),
+  ),
+
+  route('/s?/:shortCode', 'routes/redirect/index.tsx'),
+
+  route('*', 'routes/not-found.tsx'),
+] satisfies RouteConfig;
