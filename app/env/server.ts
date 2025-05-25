cc430e069be77fcd4a0aaa737560f5229e08ecee
novanish <anishchhetri196@gmail.com>
2025-05-25 20:52:48 +0545

    add imagekit env var

--- app/env/server.ts
+++ app/env/server.ts
@@ -9,6 +9,9 @@ const envSchema = z.object({
   GOOGLE_CLIENT_SECRET: z.string(),
   RESEND_API_KEY: z.string(),
   RESEND_EMAIL_DOMAIN: z.string(),
+  IMAGEKIT_PUBLIC_KEY: z.string(),
+  IMAGEKIT_PRIVATE_KEY: z.string(),
+  IMAGEKIT_URL_ENDPOINT: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
